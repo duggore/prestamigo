e3fd949f4f9c2c772c92d2634d6c1fb66e559002
Champiiy <albert1047@hotmail.com>
2016-03-18 11:41:48 -0600

    Actualizacion

---
+++ html/adminuser/interrino.php
@@ -0,0 +1,35 @@
+<?php include(HTML_DIR.'dise-secu/header.php'); ?>
+<body>
+	<section class="container-fluid">
+		<div class="row">
+			<aside class="col-xs-12  col-sm-4  col-md-3">
+				<h3>Menu</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium enim nihil iure. Maxime, autem ducimus repudiandae non cupiditate. Nobis iusto omnis molestias corporis reiciendis maxime ducimus dolore, voluptas odit iste.</p>
+			</aside>
+			<article class="col-xs-12  col-sm-8  col-md-9">
+				<div class="row color-personal">
+					<h3>Formulario</h3>
+				</div>
+				<div class="container">
+					<form>
+						<div class="form-group">
+							<label for="inputEmail">Nombre</label>
+							<input type="email" class="form-control"  placeholder="Email">
+						</div>
+						<div class="form-group">
+							<label for="inputPassword">Dirección</label>
+							<input type="password" class="form-control" placeholder="Contraseña">
+						</div>
+						<div class="form-group">
+							<label for="inputNum">Número Teléfono</label>
+							<input type="password" class="form-control" placeholder="Contraseña">
+						</div>
+						<button type="submit" class="btn btn-primary">Entrar</button>
+					</form>
+				</div>
+			</article>
+		</div>
+	</section>
+	<script src="view/app/boostrap/js/jquery.js"></script>
+	<script src="view/app/boostrap/js/boostrap.min.js"></script>
+</body>
