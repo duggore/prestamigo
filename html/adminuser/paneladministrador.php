0b591f42b429ee76eebd12d5475ece7f34051f93
esteban1234 <tva_libra@hotmail.com>
2016-03-18 10:59:55 -0600

    menu

--- html/adminuser/paneladministrador.php
+++ html/adminuser/paneladministrador.php
@@ -1,5 +1,22 @@
 <?php include (HTML_DIR.'dise-prin/header.php'); ?>
 
+<!-- --------script para mostrar y ocultar los divs------ -->
+   <script languaje="Javascript">   
+document.write('<style type="text/css">div.cajamenu{display: none;}</style>');  
+function MostrarOcultar(capa,enlace)  
+{  
+    if (document.getElementById)  
+    {  
+        var aux = document.getElementById(capa).style;  
+        aux.display = aux.display? "":"block";  
+    }  
+}  
+      
+</script>  
+
+
+
+
 <body>
 	<article class="panel">
 		<article class="pizquierda">
@@ -21,10 +38,41 @@
 		            <figure id="imgEstudiante">
 		               <img src="views/app/images/Cliente.jpg" alt="">
 		            </figure>
-            		<a href="" class="bien">Bienvenido</a>
+            		<p class="bien">Bienvenido</p>
          		</article>
 
+         		<a href="#" ><p id="menu">HOLA</p></a>
+        		<a  href="#"><p id="menu">HOLA</p></a>
+        		<a href="javascript:MostrarOcultar('caja1');" ><p id="menu">BUEN DIA</p></a>
+       
+       		 	<div class="cajamenu" id="caja1">
+           			<hr  width=85% />
+
+		        	<a href="#" ><p id="menu">BUENDIA 2</p></a>
+			        <a href="#" ><p id="menu">BUENDIA 2</p></a>
+			        <a href="#" ><p id="menu">BUENDIA 2</p></a>
+			        <hr width=85% />
+		        </div>
+        
+        <a href="javascript:MostrarOcultar('caja2');"><p id="menu">BUENAS TARDES</p></a>
+        	<div class="cajamenu" id="caja2">
+           			<hr  width=85% />
+
+		        	<a href="#" ><p id="menu">HOLA</p></a>
+			        <a href="#" ><p id="menu">HOLA</p></a>
+			        <a href="#" ><p id="menu">HOLA</p></a>
+			        <hr width=85% />
+		        </div>
+        <a href="#"><p id="menu">Mensajes</p></a>
+        <a href="#"><p id="menu">Cerrar sesión</p></a>
+
 		</article>
+
+
+
+
+
+
 		<article class="derechasection">hdhdhdhd</article>
 		
 
