1ddac1b221c15aa35199451804a2e429ae9c017b
esteban1234 <tva_libra@hotmail.com>
2016-03-22 13:03:35 -0600

    inicio de sesion terminado

--- html/public/goReg.php
+++ html/public/goReg.php
@@ -1,10 +1,40 @@
 
-<?php include (HTML_DIR.'dise-prin/header.php'); ?>
+<?php include (HTML_DIR.'dise-secu/header.php'); ?>
 
-<body>
+<body class="body">
 	<section class="iniciosesion">
-		
 
-		hhfhf
+	<!-- <figure class="logosesion"><img class="logos" src="views/app/images/logo.png" alt=""></figure> -->
+
+		
+			   <form id="inicios" action="" method="POST">
+			   		<div id="c1">INICIAR SESIÓN</div>
+					
+					<div id="conten">
+						
+						<div id="c2">
+							<input id="nom" class="emai" type="text" name="usuario" size="30" maxlength="40" placeholder="Usuario"  required><br><br>
+						    <input id="pas" class="emai" type="password" name="contra" size="30" maxlength="40" placeholder="Contraseña" required />
+						</div>
+						<div id="c3">
+							<input  class="boton" type="submit" value="INICIAR SESIÓN">
+							
+						</div>
+						<br>
+						<hr width="90%">
+					</div>
+
+					 <div class="registration">
+			                ¿Aún no tiene una cuenta?<br/>
+			                <a class="" href="#">
+			                    Aquí puede crear una
+			                </a>
+
+			         </div>	
+			         <br>
+				</form>
+
+			           	
+
 	</section>
 </body>
