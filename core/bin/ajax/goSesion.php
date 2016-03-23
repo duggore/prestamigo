529b0d413e5eb2bd070729df138aa17820bfc8d3
Champiiy <albert1047@hotmail.com>
2016-03-23 13:13:46 -0600

    Revert "Revert "Inicio Sesion""
    This reverts commit 9ea57404ac5f24dac4344b7581253ffc8062fd10.

---
+++ core/bin/ajax/goSesion.php
@@ -0,0 +1,29 @@
+<?php 
+	
+	if(!empty($_POST['user']) and !empty($_POST['pass'])){
+		$db = new Conexion();
+		$user = $db->real_escape_string($_POST['user']);
+		$pass = Encrypt($_POST['pass']);
+		$sql = $db->query("SELECT id FROM users WHERE (users ='$data' or email='$data') AND pass = '$pass' LIMIT 1;");
+			if($db->rows($sql) > 0)
+			{
+				
+			}
+
+			else{
+				echo '<div class="alert alert-dismissible alert-danger">
+	  			  	<button type="button" class="close" data-dismiss="alert">&times;</button>
+	               	<strong>Error</strong> Las credenciales son incorrectas.
+	             	</div>';
+			}
+
+		$db->close();
+	}
+
+	else{
+			echo '<div class="alert alert-dismissible alert-danger">
+	  			  <button type="button" class="close" data-dismiss="alert">&times;</button>
+	              <strong>Error</strong> Todos los datos deben estar llenos.
+	              </div>';
+	}
+?>
---
+++ html/dise-prin/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="view/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	
+</body>
+</html>
