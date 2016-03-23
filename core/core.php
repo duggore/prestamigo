c0848811d23bcb4bccd66b6d91fa80d026040790
Champiiy <albert1047@hotmail.com>
2016-03-23 13:05:04 -0600

    Sesion

--- core/core.php
+++ core/core.php
@@ -1,20 +1,6 @@
 <?php  
 	// Nucleo de la Aplicacion
-
-	#CONTANSTES DE CONEXION
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'sistemprestamo');
-
-
-
 	define('HTML_DIR', 'html/');
 	define('APP_TITLE', 'Prestamigo');
 	// define('APP_URL', 'http://localhost/Github/prestamigo');
-
-	#ESTRUCTURA
-	require('vendor/autoload.php');
-	require('core/models/class.Conexion.php');
-	require('core/bin/functions/Encrypt.php');
 ?>
