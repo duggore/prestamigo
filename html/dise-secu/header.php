ccba10fd71bd0f113cf58115f626ce2a3bc0f6dd
Champiiy <albert1047@hotmail.com>
2016-03-24 08:17:53 -0600

    Line commented

--- html/dise-secu/header.php
+++ html/dise-secu/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>hola</title>
-	<link rel="stylesheet" href="views/bootstrap/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" href="views/bootstrap/css/bootstrap.min.css"> -->
 	<link rel="stylesheet" href="views/app/css/estilo.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
